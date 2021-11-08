1532cb324ef654393d46777d028e311033964899
Patonai Dominik <patonaidominik@gmail.com>
2021-11-08 21:54:53 +0100

    init-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weather-forecast';
+
+  ngOnInit() {
+    if (navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(`lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`)
+      })
+    }
+  }
 }
