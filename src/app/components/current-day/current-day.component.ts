31e99d26614b7923b67f4473a51976978028326e
Patonai Dominik <patonaidominik@gmail.com>
2021-11-10 23:31:14 +0100

    future-days done

--- src/app/components/current-day/current-day.component.ts
+++ src/app/components/current-day/current-day.component.ts
@@ -21,9 +21,7 @@ export class CurrentDayComponent implements OnInit {
 
 
   //Adat tárolók
-  hourlyWeatherArray: any;
-  dailyWeatherArray: any;
-  locationArray: any = []
+  weatherArray: any;
 
   constructor(private forecastService: ForecastService) {
     this.dayThreshold.start.setHours(0, 0, 0);
@@ -41,65 +39,48 @@ export class CurrentDayComponent implements OnInit {
   selectCity() {
     this.locationError = false;
     this.forecastService.getHourlyForecast(this.city).subscribe(data => {
-        this.getCurrentDayForecast(data);
-      },
-      error => {
-        this.locationError = true;
-      });
-    this.forecastService.getWeatherForecast(this.city).subscribe(data => {
         // this.getCurrentDayForecast(data);
       },
       error => {
         this.locationError = true;
       });
-  }
-
-  dateRange() {
-    const start = new Date();
-    const to = new Date(start);
-    to.setHours(to.getHours() + 2, to.getMinutes() + 59, to.getSeconds() + 59);
-
-    return {start, to}
-  }
-
-  getCurrentDayForecast(data: any) {
-    console.log(data)
-    console.log(this.currentTime.getHours())
-    this.location = data.city
-    for (let datum of data.hourly) {
-      const apiDate = new Date(datum.dt_txt).getDate();
-      if (apiDate > this.dayThreshold.start.getDate()){
-        console.log(datum.dt_txt)
-      }
-    }
-    this.hourlyWeatherArray = data.hourly
+    this.forecastService.getWeatherForecast(this.city).subscribe(data => {
+        this.getCurrentDayForecast(data);
+      },
+      error => {
+        this.locationError = true;
+      });
   }
 
   // getCurrentDayForecast(data: any) {
-  //   this.location = data.city;
-  //   const timelineTemp = []
-  //
-  //   for (const forecast of data.list.slice(0, 8)) {
-  //     timelineTemp.push({
-  //       time: forecast.dt_txt,
-  //       temp: forecast.main.temp,
-  //       weather: forecast.weather.main
-  //     });
-  //     const apiDate = new Date(forecast.dt_txt).getTime();
-  //
-  //
-  //
-  //     if (this.dateRange().start.getTime() <= apiDate && this.dateRange().to.getTime() >= apiDate) {
-  //       this.weatherNow = forecast;
-  //       console.log(this.weatherNow)
-  //       console.log(data.list[0])
+  //   console.log(data)
+  //   console.log(this.currentTime.getHours())
+  //   this.location = data.city
+  //   for (let datum of data.hourly) {
+  //     const apiDate = new Date(datum.dt_txt).getDate();
+  //     if (apiDate > this.dayThreshold.start.getDate()){
+  //       console.log(datum.dt_txt)
   //     }
   //   }
-  //   this.timeline = timelineTemp;
+  //   this.hourlyWeatherArray = data.hourly
   // }
 
+  getCurrentDayForecast(data: any) {
+    console.log(data)
+    this.location = data.city;
+    const timelineTemp = []
+
+    this.weatherArray = data
+    for (const forecast of data.list.slice(0, 8)) {
+      timelineTemp.push({
+        time: forecast.dt_txt
+      });
+    }
+    this.timeline = timelineTemp;
+  }
+
   consoleLog() {
-    console.log(this.hourlyWeatherArray[0].temp)
+    console.log(this.weatherArray[0].temp)
   }
 
 }
--- src/app/components/future-days/future-days.component.ts
+++ src/app/components/future-days/future-days.component.ts
@@ -9,43 +9,55 @@ import {ForecastService} from "../../services/forecast.service";
 })
 export class FutureDaysComponent implements OnInit {
 
-  selectedHour:any;
+  selectedHour: any;
   locationError = false
   city = "";
   timeline: any = [];
   currentTime = new Date();
-  dayThreshold = ({
-    start: new Date(),
-    end: new Date()
-  });
 
   location: any;
-  weatherNow: any;
+  weatherArray: any;
+  dayN: number;
 
-  date: string = this.route.snapshot.params['dateIndex'];
 
   constructor(private forecastService: ForecastService,
               private route: ActivatedRoute) {
-    for (let i = this.currentTime.getHours(); i < 24; i++) {
-      console.log(i)
-      this.timeline.push(i)
-      console.log(this.timeline.length)
-    }
+    this.dayN = this.route.snapshot.params['dateIndex'];
   }
 
   ngOnInit(): void {
-    this.forecastService.getWeatherForecast(this.city).subscribe(data => {
-        this.getCurrentDayForecast(data);
-      },
-      error => {})
-  }
-
-  getCurrentDayForecast(data:any) {
-
+    this.selectCity()
+    this.currentTime.setDate(this.currentTime.getDate() + (+this.dayN));
   }
 
   selectCity() {
+    this.locationError = false;
+    this.forecastService.getHourlyForecast(this.city).subscribe(data => {
+        // this.getCurrentDayForecast(data);
+      },
+      error => {
+        this.locationError = true;
+      });
+    this.forecastService.getWeatherForecast(this.city).subscribe(data => {
+        this.getFutureDayForecast(data);
+      },
+      error => {
+        this.locationError = true;
+      });
+  }
 
+  getFutureDayForecast(data: any) {
+    console.log(data)
+    this.location = data.city;
+    const timelineTemp = []
+
+    this.weatherArray = data.list.slice(this.dayN * 8, 8 + (this.dayN * 8))
+    for (const forecast of data.list.slice(8, 16)) {
+      timelineTemp.push({
+        time: forecast.dt_txt
+      });
+    }
+    this.timeline = timelineTemp;
   }
 
 }
